1a7d94818a1d5d4c2a7a93c92ed6c99394c0f62d
jklaiven <87581758+jklaiven@users.noreply.github.com>
2024-08-31 13:47:31 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+base = float(input("Digite o valor da base do retangulo: "))
+altura = float(input("Digite o valor da altura do retangulo: "))
+
+area = base * altura
+
+print("A área do retângulo é:", area)
